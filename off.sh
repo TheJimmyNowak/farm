db7364d79ecc419179262392814bb041bfbe8d7d
miki164 <miki3867@gmail.com>
2020-05-01 19:36:58 +0100

    initial commit

---
+++ off.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Common path for all GPIO access
+BASE_GPIO_PATH=/sys/class/gpio
+
+# Assign names to GPIO pin numbers for each light
+RED=23
+
+# Assign names to states
+OFF="0"
+
+if [ ! -e $BASE_GPIO_PATH/gpio$RED ]; then
+  echo $RED > $BASE_GPIO_PATH/export
+fi
+
+# Utility function to set a pin as an output
+echo "out" > $BASE_GPIO_PATH/gpio$RED/direction
+echo $OFF > $BASE_GPIO_PATH/gpio$RED/value
+echo "Lights off"
